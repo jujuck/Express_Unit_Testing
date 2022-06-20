7ac35b8977fc611c53d213bc83043f40f6284038
Julien ichard WCS <julien.y.richard@gmail.com>
2022-06-20 14:29:54 +0200

    Creating an app.js for testing

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const connection = require('../db-config');
 
+connection.connect((err) => {
+  if (err) console.log('Error connecting to database', err);
+  else console.log('Connected as id ' + connection.threadId);
+});
 
+const app = express();
+app.use(express.json());
+
+// Please keep this module.exports app, we need it for the tests !
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const route = require('./route');
-const { notFound, errorHandler } = require('./middlewares');
+const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
-
-if (process.env.NODE_ENV !== 'test') {
-  app.use(morgan('dev'));
-}
-
-app.use('/api', route);
-
-app.use(notFound);
-app.use(errorHandler);
 
 app.listen(PORT, (err) => {
   // eslint-disable-next-line no-console
